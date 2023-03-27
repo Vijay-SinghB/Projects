b7d85629d754ad0cec3e0697dcc9f55d41d6abed
Vijay-SinghB <128675298+Vijay-SinghB@users.noreply.github.com>
2023-03-27 11:59:43 +0530

    Update robottasks.py

--- robottasks.py
+++ robottasks.py
@@ -1,32 +1,22 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '''
-    <!DOCTYPE html>
-<html>
-<head>
-    <title>Get Username</title>
-</head>
-<body>
-    <h1>What's your name?</h1>
-    <form method="POST">
-        <input type="text" name="name">
-        <input type="submit" value="Submit">
-    </form>
-</body>
-</html>
-    '''
-    
-@app.route('/', methods=['POST'])
-def process_form():
-    name = request.form['name']
-    message = f'Hello, {name}!'
-    return f'<script>alert("{message}"); window.location.href="/";</script>'
-    
-if __name__ == '__main__':
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    event_type = data["event_type"]
+    if event_type in ["process run", "Step run", "assistant run", "Robot run agent"]:
+        # Replace "ITS_Enable" with the name of your assistant robot
+        robot_name = "ITS_Enable"
+        response = requests.post(f"https://api.robocorp.com/start-run/{robot_name}", json=data)
+        if response.status_code == 200:
+            return "Robot run started."
+        else:
+            return f"Failed to start robot run. Error: {response.text}", 500
+    else:
+        return f"Unsupported event type: {event_type}", 400
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0')
-
-
