577118819e3dc5a24858bef71581307c774c53d1
Vijay-SinghB <128675298+Vijay-SinghB@users.noreply.github.com>
2023-03-27 11:30:17 +0530

    Update robottasks.py

--- robottasks.py
+++ robottasks.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
-
-if __name__ == "__main__":
+@app.route('/')
+def index():
+    return '''
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Get Username</title>
+</head>
+<body>
+    <h1>What's your name?</h1>
+    <form method="POST">
+        <input type="text" name="name">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+    '''
+    
+@app.route('/', methods=['POST'])
+def process_form():
+    name = request.form['name']
+    message = f'Hello, {name}!'
+    return f'<script>alert("{message}"); window.location.href="/";</script>'
+    
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
