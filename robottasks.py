d87fd25137179c782fb934cffa0a016b430a975e
Vijay-SinghB <128675298+Vijay-SinghB@users.noreply.github.com>
2023-03-27 14:10:34 +0530

    Update robottasks.py

--- robottasks.py
+++ robottasks.py
@@ -1,16 +1,18 @@
-from flask import Flask, request,abort
+from flask import Flask, request, abort
 import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-        if request.method == 'POST':
-            print(request.json)
-            return 'success', 200
-        else:
-            abort(400)
-    
+    if request.method == 'POST':
+        data = json.loads(request.data)
+        event = data['event']
+        print(event)
+        return 'success', 200
+    else:
+        abort(400)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
